e8550832182805cd5d5f80ac0f4138f6fbb4550f
Joe Hanish <joe.hanish@gmail.com>
2020-10-29 19:50:57 -0500

    s1e1

---
+++ MCTT/variables/variable_play.py
@@ -0,0 +1,9 @@
+# replace with your birth year!
+birth_year = 2000
+# replace with your birth date!
+birth_date = 10
+# replace with your birth month!
+birth_month = 'January'
+# replace with your own message!
+message = "Somebody's Birthday"
+
